73332c44e8bb96da820cfc367c3dfb58efdbc7f8
Eugene <eugene.gubar@gmail.com>
2017-03-15 01:07:28 +0200

    OOP

---
+++ oop.js
@@ -0,0 +1,68 @@
+
+'use strict'
+
+/**
+ * ---------------------- This is simple test OOP based on JavaScript ----------------------
+ */
+
+/**
+ * This is class equivalent class in other languages ex.: C#
+ * @param {string} name 
+ * @param {number} age 
+ * @param {string} answer 
+ */
+function User(name, age, answer) {
+
+    this.name   = name,
+    this.age    = age,
+    this.answer = answer;
+
+    if (this.answer === 'red') {
+        this.car = function () {
+            document.write('Hello it`s red Car. Nice choice!');
+        }
+    } else if (this.answer === 'green') {
+        this.tree = function () {
+            document.write('Hello it`s a green Tree. Good choice!');
+        }
+    } else {
+        this.say = function () {
+            document.write('Hello. You are not choice nothing. GoodBye!');
+        }
+    }
+
+}
+
+/**
+ * This is add method (static) in function construcor (class),
+ * methods the constructor function, not associated with any specific object.
+ * Method checkUser return true if user enter data (name and age)
+ * in other cases, returns false.
+ * @param   {object} user
+ * @returns {boolean}
+ */
+User.checkUser = function (user) {
+    if (user.name !== '' && user.age !== '') {
+        if (user.name !== null && user.age !== null) {
+            return true;
+        } else {
+            return false
+        }
+    } else {
+        return false;
+    }
+};
+
+/**
+ * This is prototype, from which are inherited by the created objects.
+ */
+User.prototype.infoUser = function () {
+    document.write('Name: ' + this.name + ' Age: ' + this.age);
+}
+
+var name   = prompt('Please enter your name: ');
+var age    = prompt('Please enter your age: ');
+var answer = prompt('Please choice \'red\' or \'green\' color: ');
+
+var human = new User(name, age, answer);
+
